d7e4e13344166e474eb0a8cc524c7235883fc310
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-10 14:16:14 +1100

    B: Making Radio work better

--- project-B/radio.py
+++ project-B/radio.py
@@ -6,7 +6,8 @@ ball = [0,0]
 def draw_display(ball,paddle):
     display.clear()
     display.set_pixel(paddle,0,9)
-    display.set_pixel(ball[0],ball[1],9)
+    if ball[1] > 4:
+        display.set_pixel(4-ball[0],4-((ball[1])-5),9)
   
 while 1:
   msg = radio.receive()
@@ -17,8 +18,7 @@ while 1:
         ball[0] = int(msg[1])
         ball[1] = int(msg[2])
         paddle = int(msg[3])
-    if ball[1] > 4:
-        draw_display([4-ball[0],4-((ball[1])-5)],paddle)
+        draw_display(ball,paddle)
     else:
         display.clear()
     if button_a.was_pressed():
