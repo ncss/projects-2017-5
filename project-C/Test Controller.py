1bc9abeacf40b1e01f7d08e797d83e6c9a072639
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-10 12:25:04 +1100

    Group C - Completed Test Controller
    Moves Rover forward or back for 2 seconds

--- project-C/Test Controller.py
+++ project-C/Test Controller.py
@@ -1,5 +1,20 @@
 from microbit import *
 import radio
 
+PREFIX = "ROVER:"
+
 radio.on()
 radio.config(channel=5)
+
+while True:
+    if button_a.was_pressed():
+        radio.send(PREFIX + "FORWARD:2")
+        display.show(Image.ARROW_N)
+        sleep(500)
+        
+    if button_b.was_pressed():
+        radio.send(PREFIX + "REVERSE:2")
+        display.show(Image.ARROW_N)
+        sleep(500)
+    
+    display.clear()
