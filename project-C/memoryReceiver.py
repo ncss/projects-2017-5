854ffabc6677fd1499460d57187d5705a8736de9
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-10 12:06:54 +1100

    Group C: Receiver: changes

--- project-C/memoryReceiver.py
+++ project-C/memoryReceiver.py
@@ -4,32 +4,36 @@ import radio
 radio.on()
 radio.config(channel=5)
 
-INPUTSEQUENCE = 'AB'
+INPUTSEQUENCE = ''
 SENTSEQUENCE = ''
 
 while True:
    msg = radio.receive() 
    
-   if msg and msg.startswith("MEM:"):
-       INPUTSEQUENCE = ''
-       SENTSEQUENCE = msg[4:]
+   if msg: 
+       print('1', msg)
+       if msg.startswith("MEM:"):
+           print('2', msg)
+           
+           INPUTSEQUENCE = ''
+           SENTSEQUENCE = msg[4:]
    
-   while len(INPUTSEQUENCE) < len(SENTSEQUENCE):
-       if button_a.was_pressed():
-           INPUTSEQUENCE += 'A'
-           display.show("A")
-       elif button_b.was_pressed():
-           INPUTSEQUENCE += 'B'
-           display.show("B")
-   
-   if INPUTSEQUENCE == SENTSEQUENCE:
-       radio.send('MEM:CORRECT')
-       #radio.send(ROVER:FORWARD)
-       #radio.send(ROVER:REVERSE)
-       display.show(Image.HEART)
-       sleep(500)
-       display.clear()
-   else:
-       display.show('X')
-       sleep(2000)
-       display.clear()
+       while len(INPUTSEQUENCE) < len(SENTSEQUENCE):
+           if button_a.was_pressed():
+               INPUTSEQUENCE += 'A'
+               display.show("A")
+           elif button_b.was_pressed():
+               INPUTSEQUENCE += 'B'
+               display.show("B")
+       
+       if INPUTSEQUENCE == SENTSEQUENCE:
+           radio.send('MEM:CORRECT')
+           #radio.send(ROVER:FORWARD)
+           #radio.send(ROVER:REVERSE)
+           display.show(Image.HEART)
+           sleep(500)
+           display.clear()
+       else:
+           display.show('X')
+           sleep(2000)
+           display.clear()
