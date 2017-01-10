0909c084be253feec46f471c4db015d664f4211d
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-11 09:28:36 +1100

    added state machine yesterday

--- project-A/right_leg.py
+++ project-A/right_leg.py
@@ -7,9 +7,40 @@ radio.config(channel=13)
 
 pos = "down"
 
+def is_leg_up():
+    z = accelerometer.get_z()
+    return z < -850
 
+def is_leg_down():
+    z = accelerometer.get_z()
+    return -300 < z  < -50    
 
-while True:
+STATE_START = 0
+STATE_WAIT_FOR_HOLD = 1
+STATE_HOLD_LONG_ENOUGH = 2
+STATE_SUCCESSFUL_LEG_RAISE = 3
+STATE_FINISHED = 4
+
+state = STATE_START
+
+while True:    
+    if state == STATE_START:
+        if is_leg_up():
+            state = STATE_WAIT_FOR_HOLD
+            continue
+    elif state == STATE_WAIT_FOR_HOLD:
+        
+        if is_leg_down():
+            state = STATE_START
+            continue
+        elif 
+    elif state == STATE_HOLD_LONG_ENOUGH:
+        continue
+    elif state == STATE_SUCCESSFUL_LEG_RAISE:
+        continue
+    elif state == STATE_FINISHED:
+        continue
+    
     x, y, z = accelerometer.get_values()
     sleep(50)
     
