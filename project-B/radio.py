443d67cfb1b91f09eba4179e30bfffb459ad9fd0
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-10 12:37:08 +1100

    B: Functioning Radio
    Fixed Radio so it actually functions

--- project-B/radio.py
+++ project-B/radio.py
@@ -1,15 +1,24 @@
 from microbit import *
-
+import radio
 radio.on()
 radio.config(channel=2)
-
-def draw_display(paddle,ball):
-  display.set_pixel(paddle,4,9)
-  display.set_pixel(ball[0],ball[1],9)
+ball = [0,0]
+def draw_display(ball,paddle):
+    display.clear()
+    #display.set_pixel(paddle,4,9)
+    display.set_pixel(ball[0],ball[1],9)
   
 while 1:
-  msg = radio.recieve()
+  msg = radio.receive()
   if msg:
-    msg.split(':')
+    print(msg)
+    msg = msg.split(':')
     if msg[0] == 'US':
+        ball[0] = int(msg[1])
+        ball[1] = int(msg[2])
+    if ball[1] > 4:
+        draw_display([ball[0],-1*(5-ball[1])],1)
+    else:
+        display.clear()
+    print(ball)
       
