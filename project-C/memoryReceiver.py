e292db54f30131f8d71bf739e916889a479e1d25
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-10 11:15:38 +1100

    Re wrote code
    I lost the code and re wrote it

--- project-C/memoryReceiver.py
+++ project-C/memoryReceiver.py
@@ -9,3 +9,24 @@ SENTSEQUENCE = ''
 
 while True:
    msg = radio.receive() 
+   
+   if msg and msg.startswith("MEM:"):
+       INPUTSEQUENCE = ''
+       SENTSEQUENCE = msg[4:]
+   
+   while len(INPUTSEQUENCE) < len(SENTSEQUENCE):
+       if button_a.was_pressed():
+           INPUTSEQUENCE += 'A'
+       elif button_b.was_pressed():
+           INPUTSEQUENCE += 'B'
+   
+   if INPUTSEQUENCE == SENTSEQUENCE:
+       radio.send('MEM:CORRECT')
+       display.clear()
+       display.show(Image.HEART)
+       sleep(500)
+       display.clear()
+   else:
+       display.show('X')
+       sleep(3000)
+       display.clear()
