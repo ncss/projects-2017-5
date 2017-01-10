7204f5296411dc7bacfb694cba1a708f81ccc149
ncss2017group5 <sher9594@SHARED.SYDNEY.EDU.AU>
2017-01-11 10:08:12 +1100

    A: CHOCOLATE THUNDER radio fix
    sends message for finished and resets

--- project-A/radio_control.py
+++ project-A/radio_control.py
@@ -4,23 +4,23 @@ import radio
 radio.on()
 radio.config(channel=13)
 
-#leg up sends 'left'/'right'
 goal = 30
-leg = 'right'
-count = 0
 
-while count < goal:
-    message = radio.receive()
-    if message:
-        if message == 'left' and leg == 'left':
-            count += 1
-            leg = 'right'
-            radio.send('LIGHT')
-        elif message == 'right' and leg == 'right':
-            count += 1
-            leg = 'left'
-            radio.send('LIGHT')
-        print(count)
+while True:
+    leg = 'right'
+    count = 0
+    while count < goal:
+        message = radio.receive()
+        if message:
+            if message == 'left' and leg == 'left':
+                count += 1
+                leg = 'right'
+                radio.send('LIGHT')
+            elif message == 'right' and leg == 'right':
+                count += 1
+                leg = 'left'
+                radio.send('LIGHT')
+    radio.send('finished')
         
             
             
