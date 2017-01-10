f08c8e957c1ac99ebc5379f2f43236c000bfdc2f
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-10 17:39:49 +1100

    Project D - good

--- project-D/SSAUpdate.py
+++ project-D/SSAUpdate.py
@@ -2,7 +2,7 @@ from microbit import *
 import radio
 display.off()
 radio.on()
-radio.config(channel=6)
+radio.config(channel=9)
 
 initial_flex = pin1.read_analog()
 flex_scale = 1
@@ -18,20 +18,22 @@ def check(inf):
     initial_flex = inf[1]
     flex_scale = inf[2]
     flex = abs((p.read_analog() - initial_flex) / flex_scale)
-    print(flex)
-    if flex > 30:
+    #print(flex)
+    if flex > 10:
         #print('PRESSED')
-        
         return True
         
     else:
         #print('NOT PRESSED')
         return False
     
+
     sleep(20)
     
     
 while True:
-    for i in ["left"]:
+    for i in ["left","right","for","back"]:
         if check(pins[i]):
             print(i) 
+            radio.send(i)
+            sleep(2)
