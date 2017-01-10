812f3b3028fd85210d648c0bc82644d2c79d5012
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-10 15:37:53 +1100

    Group C: TRANS: Re-writting

---
+++ project-C/memorySequencer1.0.py
@@ -0,0 +1,48 @@
+from microbit import *
+import radio
+import random
+
+radio.on()
+radio.config(channel = 5, address=0xffffffff)
+
+letters = ['A','B']
+pattern = ''
+length = 3
+
+def display_sequence(sequence):
+    for char in sequence:      
+        display.show(char)
+        sleep(1000)
+        display.clear()
+        sleep(500)
+
+while True:
+    if button_a.was_pressed() and button_b.was_pressed():
+        for i in range(length):
+            pattern += random.choice(letters)
+        print(pattern)
+        radio.send('MEM:' + pattern)
+        display_sequence(pattern)
+        correct = False
+            
+            
+ 
+ 
+    msg = radio.receive()
+    if msg:
+        if msg == 'REC:CORRECT':
+            print(msg)
+            length += 1
+            pattern = ''
+        elif msg == 'REC:INCORRECT':
+            incorrect = True
+            print(msg)
+            while incorrect:
+                if button_a.was_pressed() and button_b.was_pressed():
+                    print(pattern)
+                    radio.send('MEM:' + pattern)   
+                    display_sequence(pattern)
+                    incorrect = False
+                    
+            
+            
