db6473a19ce1daef4bbe67265b072527b90faa57
ncss2017group5 <ncss2017group5@gmail.com>
2017-01-10 15:34:26 +1100

    B: Working Pong Game
    Fully working pong game with "A" "B" controls

--- project-B/master.py
+++ project-B/master.py
@@ -4,32 +4,58 @@ import radio
 radio.on()
 radio.config(channel = 2)
 
+ticks=0
+
 ball = [1,2]
 velocity = [1,1]
  
 walls_x = [-1,5]
-walls_y = [-1,10]
+walls_y = [0,9]
 
 paddle_1 = 2 #Paddle 1 is master paddle
 paddle_2 = 2
 
-def move_ball():
-    if ball[0] + velocity[0] in walls_x: #Needs to take into account paddle
+def move_ball(): 
+    if ball[0] + velocity[0] in walls_x: #Needs to take into account paddle 
         velocity[0] = -velocity[0]
+    if ball[0] == paddle_1:
+        if ball[1] + velocity[1] == 0:
+            velocity[1] = -velocity[1]
+    elif ball[0] == 4-paddle_2: 
+        if ball[1] + velocity[1] == 9:    
+            velocity[1] = -velocity[1]
+    
     ball[0] += velocity[0]
-   
+    ball[1] += velocity[1] 
+    """
+    if ball[0] + velocity[0] in walls_x: #Needs to take into account paddle 
+        velocity[0] = -velocity[0] 
+    ball[0] += velocity[0] 
+    
+    
     if ball[1] + velocity[1] in walls_y:
-        velocity[1] = -velocity[1]
-    ball[1] += velocity[1]
+        print(paddle_1)
+        print(paddle_2)
+        print(ball[0])
+    
+    if ball[1] + velocity[1] == 0 and paddle_1 == 4-ball[0]: 
+        velocity[1] = -velocity[1]  
+    elif ball[1] + velocity[1] == 9 and paddle_2 == 4-ball[0]: 
+        velocity[1] = -velocity[1] 
+    ball[1] += velocity[1] 
 
+    """
 def shift_paddle(paddle, direction):
     if paddle + direction not in walls_x:
       paddle += direction
+    return paddle
 
 current_screen = 1      
       
 def update_screen():
+  display.clear()
   #radio.send("US:" + ball[0] + ":" + ball[1])#add paddle
+  display.set_pixel(paddle_1,0,9)
   if current_screen == 1:
     display_range = [0,1,2,3,4]
     if ball[1] in display_range:
@@ -40,32 +66,39 @@ def update_screen():
         display.set_pixel(ball[0],ball[1]-5, 9 )
       
 
-
-
+def end_game(winner):
+    if winner == '1':
+        display.show(Image('90009:09990:00000:09090:09090'),loop=True)
+    elif winner == '0':
+        display.show(Image('09990:90009:00000:09090:09090'),loop=True)
   
 while 1: 
-    move_ball()
-    radio.send("US:" + str(ball[0]) + ":" + str(ball[1]) + ":" + str(paddle_2))#add paddle
+    if ticks % 30 == 0:
+        move_ball()
+       
     update_screen()
-     
+    radio.send("US:" + str(ball[0]) + ":" + str(ball[1]) + ":" + str(paddle_2))#add paddle
     
-    display.set_pixel(paddle_1,0,9)
+    if ball[1] == 0:
+        end_game('1')
+    if ball[1] == 9:
+        end_game('0')
     
-    if button_a.was_pressed:
-        shift_paddle(paddle_1, -1)
-    if button_b.was_pressed:
-        shift_paddle(paddle_1, 1)
+    if button_a.was_pressed():
+        paddle_1 = shift_paddle(paddle_1, -1)
+    if button_b.was_pressed():
+        paddle_1 = shift_paddle(paddle_1, 1)
     
     b = radio.receive()
     if b:
-        b = b.split()
+        b = b.split(":")
         if b[0] == "MP":
             if b[2] == "r":
-                shift_paddle(paddle_2, -1)
+                paddle_2 = shift_paddle(paddle_2, 1)
             if b[2] == "l":
-                shift_paddle(paddle_2, 1)
-    sleep(300)
-    display.clear()
+                paddle_2 = shift_paddle(paddle_2, -1)
+    sleep(10)
+    ticks += 1
     
     
     
