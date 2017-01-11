9e89a2f76303df80c9fb0f30cc1c15001555cb7a
ncss2017group5 <sit_ncss@SHARED.SYDNEY.EDU.AU>
2017-01-11 14:26:33 +1100

    A: added the neopixel board

---
+++ project-A/neopixel_board.py
@@ -0,0 +1,23 @@
+from microbit import *
+import neopixel
+import radio
+
+radio.on()
+radio.config(channel=13)
+
+np = neopixel.NeoPixel(pin0, 30)
+
+pixels = [(min(255,i*12),max(0,255-i*12),0) for i in range(30)]
+
+print(pixels)
+
+for i in range(30):
+    while True:
+        message = radio.receive()
+        if message:
+            if message == 'LIGHT':
+                print('got message')
+                #if message == 'LIGHT':
+                np[i] = pixels[i]
+                np.show()
+                break
